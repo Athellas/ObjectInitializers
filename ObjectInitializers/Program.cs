8bd074f714df3a90043ff834aa0536c22d3a1e95
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-02-26 17:26:10 +0100

    Commit summary
    commit1

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -10,6 +10,27 @@ namespace ObjectInitializers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Init Syntax *****\n");
+
+            // manually setting properties
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            // using custom ctor
+            Point anotherPoint = new Point();
+            anotherPoint.DisplayStats();
+
+            // init syntax
+            Point finalPoint = new Point {X = 30, Y = 30};
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+
+            Point goldPoint = new Point(PointColor.Gold) { X=90, Y= 20};
+            goldPoint.DisplayStats();
+            Console.ReadLine();
+
         }
     }
 }
